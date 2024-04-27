cc6c398aedc438bf6ae27f6e03c98032b51e295d
Augusto Polonio <augustodrp@gmail.com>
2024-04-27 12:41:00 -0300

    [feat] Base Grid/Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table"
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="p-6 max-w-4xl mx-auto">
+      <div className="border rounded">
+      <Table>
+        <TableHeader>
+            <TableHead>ID</TableHead>
+            <TableHead>Produto</TableHead>
+            <TableHead>Preço</TableHead>
+        </TableHeader>
+
+        <TableBody>
+          {Array.from({ length: 10 }).map((_, i) => {
+            return (
+              <TableRow key={i}>
+                <TableCell>{crypto.randomUUID()}</TableCell>
+                <TableCell>Produto 1</TableCell>
+                <TableCell>R$ 192,00</TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+      </div>
+    </div>
   )
 }
 
