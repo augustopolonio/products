2bd8748fe67189edaaa77789b80fe073013c7808
Augusto Polonio <augustodrp@gmail.com>
2024-04-27 13:03:07 -0300

    [feat] Top bar/form with filters and add product button

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
+import { Button } from "./components/ui/button"
+import { Input } from "./components/ui/input"
+import { Search, PlusCircle } from "lucide-react"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./components/ui/table"
 
 export function App() {
   return (
-    <div className="p-6 max-w-4xl mx-auto">
+    <div className="p-6 max-w-4xl mx-auto space-y-4">
+      <h1 className="text-3xl font-bold">Products</h1>
+
+      <div className="flex items-center justify-between">
+        <form className="flex items-center gap-2">
+          <Input name="id" placeholder="Product ID" className="w-auto"></Input>
+          <Input name="id" placeholder="Product Name" className="w-auto"></Input>
+          <Button variant={"outline"}>
+            <Search className="w-4 h-4 mr-2"></Search>
+            Filter
+          </Button>
+        </form>
+
+        <Button>
+          <PlusCircle className="w-4 h-4 mr-2"></PlusCircle>
+          New Product
+        </Button>
+      </div>
+
       <div className="border rounded">
-      <Table>
-        <TableHeader>
+        <Table>
+          <TableHeader>
             <TableHead>ID</TableHead>
             <TableHead>Produto</TableHead>
             <TableHead>Preço</TableHead>
-        </TableHeader>
+          </TableHeader>
 
-        <TableBody>
-          {Array.from({ length: 10 }).map((_, i) => {
-            return (
-              <TableRow key={i}>
-                <TableCell>{crypto.randomUUID()}</TableCell>
-                <TableCell>Produto 1</TableCell>
-                <TableCell>R$ 192,00</TableCell>
-              </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
+          <TableBody>
+            {Array.from({ length: 10 }).map((_, i) => {
+              return (
+                <TableRow key={i}>
+                  <TableCell>{crypto.randomUUID()}</TableCell>
+                  <TableCell>Produto 1</TableCell>
+                  <TableCell>R$ 192,00</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
       </div>
     </div>
   )
